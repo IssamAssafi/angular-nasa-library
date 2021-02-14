505f478a4b507295f92d14560941c5bb06586236
Issam Assafi <issamassafi@gmail.com>
2021-02-14 21:18:42 +0100

    deploy heroku fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/botmind-app'));
+app.use(express.static(__dirname + '/dist/nasa-library'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/botmind-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/nasa-library/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
